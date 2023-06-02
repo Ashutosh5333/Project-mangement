e30c5f43d07f2211d11b24fc3371fcc75376bcb6
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-06-02 12:26:28 +0530

    Added count and Status

--- Server/routes/Project.route.js
+++ Server/routes/Project.route.js
@@ -6,7 +6,6 @@ const ProjectRouter = express.Router()
 
 
  ProjectRouter.get("/project", async (req,res) =>{
-
       const Projectdata = await ProjectModel.find()
      res.send(Projectdata)
  })
@@ -23,5 +22,105 @@ const ProjectRouter = express.Router()
          }
             
  })
+
+ // -----------------  Cancel run  -----------------
+
+ProjectRouter.patch('/statusrun/:id', async (req, res) => {
+   try {
+     const updatedData = await ProjectModel.findByIdAndUpdate(req.params.id, { Status: 'running' }, { new: true });
+     res.json(updatedData || { error: 'Project not found' });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+
+  // -----------------  Cancel Close  -----------------
+
+ ProjectRouter.patch('/statusclose/:id', async (req, res) => {
+   try {
+     const updatedData = await ProjectModel.findByIdAndUpdate(req.params.id, { Status: 'Closed' }, { new: true });
+     res.json(updatedData || { error: 'Project not found' });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+
+  // -----------------  Cancel Status  -----------------
+
+
+ ProjectRouter.patch('/statuscancel/:id', async (req, res) => {
+   try {
+     const updatedData = await ProjectModel.findByIdAndUpdate(req.params.id, { Status: 'Cancelled' }, { new: true });
+     res.json(updatedData || { error: 'Project not found' });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+ 
+ // -----------------  Total projects -----------------
+
+ ProjectRouter.get('/totalprojects', async (req, res) => {
+   try {
+     const totalCount = await ProjectModel.countDocuments();
+     res.json({ totalProjects: totalCount });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+ 
+
+ // -----------------  Cancell -----------------
+
+ ProjectRouter.get('/canceledproject', async (req, res) => {
+   try {
+     const canceledCount = await ProjectModel.countDocuments({ Status: 'Cancelled' });
+     res.json({ canceledProject: canceledCount });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+
+
+ // -----------------  RUNNING -----------------
+
+
+ ProjectRouter.get('/runningproject', async (req, res) => {
+   try {
+     const canceledCount = await ProjectModel.countDocuments({ Status: 'running' });
+     res.json({ RunningProject: canceledCount });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+ 
+
+      //   -----------  closed  ---------------- 
+
+
+ ProjectRouter.get('/closedproject', async (req, res) => {
+   try {
+     const canceledCount = await ProjectModel.countDocuments({ Status: 'closed' });
+     res.json({ ClosedProject: canceledCount });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+
+
+       //  -------------- Registered  --------------- 
+       
+       
+ ProjectRouter.get('/Registered', async (req, res) => {
+   try {
+     const canceledCount = await ProjectModel.countDocuments({ Status: 'Registered' });
+     res.json({ ClosedProject: canceledCount });
+   } catch (error) {
+     res.status(500).json({ error });
+   }
+ });
+
+
+
+
 
  module.exports={ProjectRouter}
